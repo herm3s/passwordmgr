bc92e9798d3bf9cca274f0a08a0d060bbc583982
herm3s <angeluster@gmail.com>
2019-06-18 09:14:42 +0700

    update pass
    update pass

--- src/constants.js
+++ src/constants.js
@@ -5,7 +5,7 @@
     PROJECT_ID: 'passwordmgr', // ใส่ projectId fore Firebase
     KEY_PATH: './src/key.json' // ที่อยู่ของ key json ของ firebase ไฟล์
   },
-  URL_COPY_PASSWORD: 'https://example.com/v1/api/copy?text=', // แก้ไข url ที่ deploy เพื่อใช้งานจริงได้เลย
+  URL_COPY_PASSWORD: 'https://pwrdmgr.herokuapp.com/v1/api/copy?text=', // แก้ไข url ที่ deploy เพื่อใช้งานจริงได้เลย
   URL_LOGO: 'https://images2.imgbox.com/7e/0b/Cz8AV24h_o.png',
   BOT_MSG: {
     ACCOUNT: 'กรุณาระบุชื่อ Account ที่ต้องการ',
